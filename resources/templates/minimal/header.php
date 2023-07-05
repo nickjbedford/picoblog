d01e3db60666ce9a711a6cb9abd18cd36dfb64a5
Nick Bedford <nick.bedford@healthia.com.au>
2023-07-05 15:21:46 +1000

    Imported JetBrains Mono font into styles.css.

--- resources/templates/minimal/header.php
+++ resources/templates/minimal/header.php
@@ -10,7 +10,6 @@
 		<title><?= htmlentities($document->title) ?> &mdash; <?= Config::get('site.title') ?></title>
 		<link rel="preconnect" href="https://fonts.googleapis.com" />
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-		<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet" />
 		<link rel="stylesheet" href="/styles/templates/minimal/styles.css?ver<?= filemtime(PUBLIC_PATH . '/styles/templates/minimal/styles.css') ?>" />
 	</head>
 	<body>
